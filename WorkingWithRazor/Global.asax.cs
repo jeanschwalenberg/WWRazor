cca789a97ae14a59a1662f1d3494ca35e4d90e25
JeanSchwalenberg <jeanschwalenberg@gmail.com>
2016-06-24 20:27:53 -0600

    Initial commit

--- WorkingWithRazor/Global.asax.cs
+++ WorkingWithRazor/Global.asax.cs
@@ -4,15 +4,17 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using WorkingWithRazor.Infrastructure;
 
-namespace WorkingWithRazor
-{
-    public class MvcApplication : System.Web.HttpApplication
-    {
-        protected void Application_Start()
-        {
+namespace WorkingWithRazor {
+
+    public class MvcApplication : System.Web.HttpApplication {
+        protected void Application_Start() {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new CustomLocationViewEngine());
         }
     }
 }
